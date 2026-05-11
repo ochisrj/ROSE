ff41d13c8632da63813eff40e934000515e8a885
ochisrj <kittiphumisarayci57@gmail.com>
2026-05-11 21:44:03 +0700

    Back to og remove AI work

--- ROSE/main.cpp
+++ ROSE/main.cpp
@@ -1,25 +1,5 @@
-﻿#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-
-#include <filesystem>
+﻿#include<filesystem>
 namespace fs = std::filesystem;
-
-#include "Core/Log.h"
-#include "Core/Engine.h"
-#include "Core/Time.h"
-#include "Core/Event.h"
-#include "Scene/SceneManager.h"
-#include "Resource/ResourceManager.h"
-#include "ECS/Entity.h"
-#include "ECS/Component.h"
-#include "ECS/System.h"
-
-#include "ShaderClass.h"
-#include "VAO.h"
-#include "VBO.h"
-#include "EBO.h"
-#include "Texture.h"
-#include "Camera.h"
 
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
@@ -32,27 +12,43 @@ namespace fs = std::filesystem;
 #include "implot3d.h"
 #include "implot3d_internal.h"
 
+#include"ShaderClass.h"
+#include"VAO.h"
+#include"VBO.h"
+#include"EBO.h"
+
+// #include "font.h"
+#include "menubar.h"
+#include"Texture.h"
+#include"Camera.h"
+
 #define GLM_ENABLE_EXPERIMENTAL
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <stb/stb_image.h>
-#include <iostream>
-#include <string>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+#include<stb/stb_image.h>
+#include <stack>
 #include <vector>
 #include <sstream>
-#include <map>
+#include <map> 
 #include <math.h>
+#include <vector>
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <string>
+
 
 GLfloat vertices[] =
-{
-    -0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f, 0.0f, 0.0f,
-    -0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f, 5.0f, 0.0f,
-     0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f, 0.0f, 0.0f,
-     0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f, 5.0f, 0.0f,
-     0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f, 2.5f, 5.0f
+{ //     COORDINATES     /        COLORS      /   TexCoord  //
+    -0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+    -0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+     0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+     0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+     0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	2.5f, 5.0f
 };
 
+// Indices for vertices order
 GLuint indices[] =
 {
     0, 1, 2,
@@ -63,42 +59,218 @@ GLuint indices[] =
     3, 0, 4
 };
 
+
+
+/*
+bool LoadTextureFromFile(const char* filename, GLuint* out_texture, int* out_width, int* out_height)
+{
+    int image_width = 0;
+    int image_height = 0;
+    unsigned char* image_data = stbi_load(filename, &image_width, &image_height, NULL, 4);
+    if (image_data == NULL)
+        return false;
+
+    GLuint image_texture;
+    glGenTextures(1, &image_texture);
+    glBindTexture(GL_TEXTURE_2D, image_texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+#if defined(GL_UNPACK_ROW_LENGTH) && !defined(__EMSCRIPTEN__)
+    glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
+#endif
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
+    stbi_image_free(image_data);
+
+    *out_texture = image_texture;
+    *out_width = image_width;
+    *out_height = image_height;
+
+    return true;
+}
+*/
+
 int main()
 {
-    auto& engine = Engine::Instance();
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    if (!engine.Init(1920, 1080, "ROSE Engine")) {
-        return -1;
-    }
+    int width = 1920;
+    int height = 1080;
+
+    GLFWwindow* window = glfwCreateWindow(width, height, "ROSE game engine", NULL, NULL);
+    if (window == NULL) { glfwTerminate(); return -1; }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(0);
+    gladLoadGL();
+    glViewport(0, 0, width, height);
+
+    Shader shaderProgram("default.vert", "default.frag");
+
+    VAO VAO1;
+    VAO1.Bind();
+
+    VBO VBO1(vertices, sizeof(vertices));
+    EBO EBO1(indices, sizeof(indices));
+
+    VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+    VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    VAO1.Unbind();
+    VBO1.Unbind();
+    EBO1.Unbind();
+
+    //GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+    // --- Initialize ImGUI & ImPlot ---
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImPlot::CreateContext();
+    ImPlot3D::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    /*
+        ImFontConfig font_cfg;
+        font_cfg.FontDataOwnedByAtlas = false;
+        io.Fonts->AddFontFromMemoryTTF(cascadiacode,cascadiacodesize, 17.0f, &font_cfg, io.Fonts->GetGlyphRangesThai());
+    */
+
 
     stbi_set_flip_vertically_on_load(true);
 
     std::string parentDir = (fs::current_path().fs::path::parent_path()).string();
     std::string texPath = "/ROSE/";
     std::string fullPath = parentDir + texPath + "20011.jpg";
+
     std::cout << "Texture Full Path: " << fullPath << std::endl;
+    Texture Student((parentDir + texPath + "20011.jpg").c_str(), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+    Student.texUnit(shaderProgram, "tex0", 0);
 
-    auto scene = std::make_unique<Scene>("DefaultScene");
-    EntityRegistry& registry = EntityRegistry::Instance();
+    /*int widthImg, heightImg, numColCh;
+    unsigned char* bytes = stbi_load("20011.jpg", &widthImg, &heightImg, &numColCh, 4);
 
-    EntityID testEntity = scene->CreateEntity("TestPyramid");
-    auto& transform = registry.GetComponent<TransformComponent>(testEntity);
-    transform.Position = glm::vec3(0.0f, 0.0f, -2.0f);
-    transform.Scale = glm::vec3(1.0f, 1.0f, 1.0f);
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture);
 
-    auto& sprite = registry.AddComponent<SpriteComponent>(testEntity);
-    sprite.TexturePath = fullPath;
-    sprite.Size = glm::vec2(1.0f, 1.0f);
-    sprite.Layer = 0;
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    ResourceManager::Instance().LoadTexture("test_texture", fullPath.c_str(), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
-    ResourceManager::Instance().LoadShader("default_shader", "default.vert", "default.frag");
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-    SceneManager::Instance().AddScene(std::move(scene));
-    SceneManager::Instance().SetActiveScene("DefaultScene");
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, widthImg, heightImg, 0, GL_RGBA, GL_UNSIGNED_BYTE, bytes);
+    glGenerateMipmap(GL_TEXTURE_2D);
 
-    engine.Run();
+    stbi_image_free(bytes);
+    glBindTexture(GL_TEXTURE_2D, 0);
 
-    engine.Shutdown();
+
+    GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+    shaderProgram.Activate();
+    glUniform1i(tex0Uni, 0);
+    */
+    //float rotation = 0.0f;
+    //double prevtime = glfwGetTime();
+
+    glEnable(GL_DEPTH_TEST);
+
+    Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
+
+    while (!glfwWindowShouldClose(window))
+    {
+
+        glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        shaderProgram.Activate();
+
+        camera.Inputs(window);
+
+        camera.Matrix(45.0f, 0.1f, 100.0f, shaderProgram, "camMatrix");
+
+        /*
+        double crntTime = glfwGetTime();
+
+        if (crntTime - prevtime >= 1 / 60)
+        {
+            rotation += 0.1f;
+            prevtime = crntTime;
+        }
+
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 proj = glm::mat4(1.0f);
+
+        model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+        view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+        proj = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
+
+        int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
+        glUniform1f(uniID, 0.5f);
+        */
+
+        //glBindTexture(GL_TEXTURE_2D, texture);
+        Student.Bind();
+        VAO1.Bind();
+
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        /*
+        GLint sizeID = glGetUniformLocation(shaderProgram, "size");
+        glUniform1f(sizeID, 1.0f);
+
+        GLint colorID = glGetUniformLocation(shaderProgram, "color");
+        glUniform4f(colorID, 1.0f, 0.5f, 0.2f, 1.0f);
+
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+        */
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        MenuBar::Draw();
+
+        glad_glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    };
+
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImPlot3D::DestroyContext();
+    ImPlot::DestroyContext();
+    ImGui::DestroyContext();
+
+    VAO1.Delete();
+    VBO1.Delete();
+    EBO1.Delete();
+    Student.Delete();
+    //glDeleteTextures(1, &texture);
+    shaderProgram.Delete();
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
