7ba630de02dce1930ab1c96bab2112b3133cec99
ochisrj <kittiphumisarayci57@gmail.com>
2026-07-20 22:00:15 +0700

    create window add triangle

--- Rose/main.cpp
+++ Rose/main.cpp
@@ -12,24 +12,27 @@
 
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"out vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   gl_Position = vec4(aPos, 1.0);\n"
+"   vertexColor = vec4(0.5,0.0,0.0,1.0);\n"
 "}\0";
 
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(0.8f, 0.3f, 0.02f, 1.0f);\n"
+"   FragColor = vertexColor;\n"
 "}\n\0";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
 // Window dimensions
-const unsigned int SCR_WIDTH = 1280;
-const unsigned int SCR_HEIGHT = 720;
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
 
 int main()
 {
@@ -89,18 +92,33 @@ int main()
         0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
         0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f // Upper corner
 
+        // Rectangle
+        // 0.5f,  0.5f, 0.0f,  // top right
+        // 0.5f, -0.5f, 0.0f,  // bottom right
+        //-0.5f, -0.5f, 0.0f,  // bottom left
+        //-0.5f,  0.5f, 0.0f   // top left 
     };
 
+    unsigned int indices[] =
+    {
+        0,1,3,
+        1,2,3
+    };
+
+    unsigned int EBO;
     GLuint VAO, VBO;
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
@@ -121,8 +139,6 @@ int main()
     float clearColor[4] = { 0.10f, 0.10f, 0.12f, 1.0f };
     bool showDemoWindow = false;
     int counter = 0;
-
-    std::cout << "Tst";
 
     // MAIN RENDER LOOP
     while (!glfwWindowShouldClose(window))
@@ -176,8 +192,10 @@ int main()
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glDrawArrays(GL_TRIANGLES, 0, 3);
-
+        glBindVertexArray(0);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // wireframe mode using GL_LINE : GL_FILL to default
 
         // --- Draw ImGui UI on top of the OpenGL scene ---
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
